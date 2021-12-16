88a693d2844b17499f3c68b92592c97fa603cd63
sanja <sanja@LAPTOP-GE3Q5QSF>
2021-12-16 22:38:08 +0530

    Add project files.

---
+++ azurecoremvc/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using azurecoremvc.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace azurecoremvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly EmployeeContext context;
+        public HomeController(ILogger<HomeController> logger, EmployeeContext _context)
+        {
+            _logger = logger;
+            context = _context;
+        }
+
+        public IActionResult Index()
+        {
+            var employees = context.employee.ToList();
+            return View(employees);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ azurecoremvc/employeedb.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace azurecoremvc
+{
+    public class EmployeeContext : DbContext
+    {
+        public DbSet<employee> employee { get; set; }
+        private readonly IConfiguration _configuration;
+        public string DbPath { get; }
+
+        public EmployeeContext(IConfiguration _config)
+        {
+            _configuration = _config;
+            DbPath = _configuration.GetConnectionString("SQLConnection");
+        }
+
+        // The following configures EF to create a Sqlite database file in the
+        // special "local" folder for your platform.
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlServer(DbPath);
+    }
+
+    public class employee
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+
+    }
+
+}
